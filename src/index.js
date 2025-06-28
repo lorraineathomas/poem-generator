9de848f2a27d58170bda37e0b6e7a092ead3ceab
Lorraine Thomas <116093083+lorraineathomas@users.noreply.github.com>
2025-06-28 19:09:09 -0400

    Added prompt for user instructions

--- src/index.js
+++ src/index.js
@@ -1,40 +1,26 @@
-function createPoem(event){
-    event.preventDefault();
-
-        new Typewriter('#poem', {
-            strings: `I doubt not God is good, well-meaning, kind,
-            <br/>
-            And did He stoop to quibble could tell why
-            <br/>
-            The little buried mole continues blind,   
-            <br/>
-            Why flesh that mirrors Him must some day die,
-            <br/>
-            Make plain the reason tortured Tantalus
-            <br/>
-            Is baited by the fickle fruit, declare   
-            <br/>
-            If merely brute caprice dooms Sisyphus
-            <br/>
-            To struggle up a never-ending stair.   
-            <br/>
-            Inscrutable His ways are, and immune   
-            <br/>
-            To catechism by a mind too strewn   
-            <br/>
-            With petty cares to slightly understand  
-            <br/> 
-            What awful brain compels His awful hand.  
-            <br/> 
-            Yet do I marvel at this curious thing:   
-            <br/>
-            To make a poet black, and bid him sing!
-            <br/>
-             - Countee Cullen`,
+function typePoem(response){
+    console.log("Poem created");
+    new Typewriter('#poem', {
+            strings: response.data.answer,
             autoStart: true,
             cursor: null,
             delay: 40
         });
+}
+
+function createPoem(event){
+    event.preventDefault();
+
+
+    let inputWord = document.querySelector("#poem-focus-input");
+    let apiKey = "73a673doafc73bccb4ff04ct60632486";
+    let prompt = `Generate a poem about ${inputWord.value}`;
+    let context = "The user will provide one word or more. The poem generated should be related to the word submitted. Please separate each sentence on separate lines. Include '- SheCodes AI' at the end.";
+    let url = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    console.log("Writing poem...")
+
+    axios.get(url).then(typePoem);
 }
 
 let submitted = document.querySelector("#poem-focus-submit");
